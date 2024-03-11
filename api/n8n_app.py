3835057e91d96181db6544b9af362c85fc4c44d6
yeo-menghan <yeomenghan1989@gmail.com>
2024-03-11 12:51:46 +0700

    Edit vercel.json build setting

--- api/n8n_app.py
+++ api/n8n_app.py
@@ -1,17 +1,13 @@
 from flask import Flask, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
-@app.route('/api')
-def home():
-    return "Hello from vercel flask"
-
-# Define the route for the GET request
-@app.route('/api/get-json', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_json():
     # Specify the path to your JSON file
-    json_file_path = 'n8n_node_list_partial.json'
+    json_file_path = os.path.join(os.path.dirname(__file__), 'n8n_node_list_partial.json')
 
     # Open and read the JSON file
     with open(json_file_path, 'r') as json_file:
